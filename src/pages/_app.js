0c1ecc1726baa21409d62af3184fbde314d50349
senali <senalidilumika@gmail.com>
2023-02-18 00:16:57 +0530

    Chakra ui install

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import Header from '@/components/NavBar'
 import Footer from '@/components/footer'
 import '@/styles/globals.css'
@@ -7,7 +8,9 @@ export default function App({ Component, pageProps }) {
     <div className="min-h-[100vh]">
       <Header />
       <div className="px-2 sm:px-4 pt-[70px]">
-        <Component {...pageProps} />
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
       </div>
       <Footer />
     </div>
