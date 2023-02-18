4a202815a423b47624288553c56f04696ecfe5a1
Neel <neelp0980@gmail.com>
2023-02-18 23:02:35 +0530

    Added arcana wallet

---
+++ src/utils/arcana.js
@@ -0,0 +1,28 @@
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { polygonMumbai } from "@wagmi/core/chains";
+import { publicProvider } from "wagmi/providers/public";
+import { ArcanaConnector } from "@arcana/auth-wagmi"
+
+const arcana_app_address = "92ed0865eadd5821c028a0bfb2604eda24d341f8";
+const connector = new ArcanaConnector({
+  chains: [polygonMumbai],
+  options: {
+    appId: `${arcana_app_address}`, // appId = App Address
+    theme: 'light',            // Defaults to 'dark'
+    alwaysVisible: false,      // Defaults to true
+    position: 'left'           // Defaults to 'right'
+  },
+})
+
+const { chains, provider } = configureChains(
+  [polygonMumbai],
+  [publicProvider()]
+); 
+
+const wagmiClient = createClient({
+    autoConnect: true,
+    connectors: connector(chains),
+    provider,
+  });
+
+export const client = wagmiClient;
