0713417b346a2f6f25a4e8abd10ebde423520ecd
Vraj Desai <43074241+vrajdesai78@users.noreply.github.com>
2023-02-19 21:04:17 +0530

    disribute funds funciton added

--- src/pages/fitness.jsx
+++ src/pages/fitness.jsx
@@ -118,6 +118,58 @@ const steps = stepsCount != 0 ? stepsCount : 1000;
    
   }, []);
 
+  async function distributeFunds(id, amount) {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+  
+    const signer = provider.getSigner();
+  
+    const chainId = await window.ethereum.request({ method: "eth_chainId" });
+    const sf = await Framework.create({
+      chainId: Number(chainId),
+      provider: provider
+    });
+  
+    const superSigner = sf.createSigner({ signer: signer });
+  
+    console.log(signer);
+    console.log(await superSigner.getAddress());
+    const daix = await sf.loadSuperToken("fDAIx");
+  
+    console.log(daix);
+  
+    try {
+      const distributeOperation = daix.distribute({
+        indexId: id,
+        amount: amount
+        // userData?: string
+      });
+  
+      console.log("Distributing...");
+  
+      await distributeOperation.exec(signer);
+  
+      console.log(
+        `Congrats - you've just distributed to an Index!
+           Network: Goerli
+           Super Token: DAIx
+           Index ID: ${id}
+           Amount: ${amount}         
+        `
+      );
+  
+      console.log(
+        `Congrats - you've just distributed to your index!
+      `
+      );
+    } catch (error) {
+      console.log(
+        "Hmmm, your transaction threw an error. Make sure that this stream does not already exist, and that you've entered a valid Ethereum address!"
+      );
+      console.error(error);
+    }
+  }
+
   return (
     <>
       <Head>
